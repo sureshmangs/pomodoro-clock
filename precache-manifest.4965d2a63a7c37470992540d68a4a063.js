d2801b844d6f0d0495aa0965aa6c30deeac59b24
sureshmangs <sureshmangs1@gmail.com>
2021-06-03 12:28:41 +0530

    Updates

---
+++ precache-manifest.4965d2a63a7c37470992540d68a4a063.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61875a6ce882b0df35c249aa0a9de51",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "c35bbfe24ab4f5d1822c",
+    "url": "/pomodoro-clock/static/css/main.048e3044.chunk.css"
+  },
+  {
+    "revision": "5929628fda43d3d1f4d4",
+    "url": "/pomodoro-clock/static/js/2.3ccbcb97.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pomodoro-clock/static/js/2.3ccbcb97.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c35bbfe24ab4f5d1822c",
+    "url": "/pomodoro-clock/static/js/main.9f15ce21.chunk.js"
+  },
+  {
+    "revision": "4cc8e959fe1d967d61ff",
+    "url": "/pomodoro-clock/static/js/runtime-main.c524c67b.js"
+  },
+  {
+    "revision": "988833bf34d92eba3d536029cd9e1bc6",
+    "url": "/pomodoro-clock/static/media/alarm.988833bf.wav"
+  }
+]);
